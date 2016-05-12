613406da8ac6003ba041ca1db9c3332cb0deb12b
Brooks Lyrette <brooks@dotsub.com>
2016-05-12 11:52:14 -0400

    Working caption plugin. Supports basic fomatting (bold, italics, underline).

--- src/plugin.js
+++ src/plugin.js
@@ -1,7 +1,116 @@
 import videojs from 'video.js';
 
 // Default options for the plugin.
-const defaults = {};
+const defaults = {
+  language: { direction: 'ltr' },
+  captions: []
+};
+
+let settings = {};
+
+const styleToTag = {
+  BOLD: ['<b>', '</b>'],
+  ITALIC: ['<i>', '</i>'],
+  UNDERLINE: ['<u>', '</u>']
+};
+
+const convertToHtml = (caption) => {
+  let displayValue = '';
+  const str = caption.content;
+
+  if (caption.inlineStyles) {
+    for (let i = 0; i <= str.length; i++) {
+      for (const style of caption.inlineStyles) {
+        if (i === style.offset) {
+          displayValue += styleToTag[style.style][0];
+        } else if (i === style.offset + style.length) {
+          displayValue += styleToTag[style.style][1];
+        }
+      }
+      if (i < str.length) {
+        displayValue += str[i];
+      }
+    }
+  } else {
+    displayValue = caption.content;
+  }
+  return displayValue.replace(/\n/g, '<br/>');
+};
+
+const updateCaption = (player) => {
+  const time = player.currentTime() * 1000;
+
+  const videoEl = player.el();
+  const captions = settings.captions.filter((caption) => {
+    return caption.start <= time && caption.end >= time;
+  });
+
+  const textElement = videoEl.getElementsByClassName('vjs-text')[0];
+  const textContainer = videoEl.getElementsByClassName('vjs-caption-containter')[0];
+
+  // TODO: There can now be two captions with the same timecode.
+  if (textElement && captions[0]) {
+    textElement.innerHTML = convertToHtml(captions[0]);
+    textContainer.classList.remove('vjs-hidden');
+  } else {
+    textElement.innerHTML = '';
+    textContainer.classList.add('vjs-hidden');
+  }
+};
+
+/**
+ * Function is used to set the captions for the plugin. Captions must be an array.
+ *
+ * Captions from v2 of Dotsub are in the form:
+ *          {start: timeInMills, duration: timeInMills, content: 'Caption Text'}
+ * Captions from v3 of Dotsub are in the form:
+ *          {start: timeInMills, end: timeInMills, content: 'Caption Text'}
+ * v3 can also contain formatting and position data {position, inlineStyles: []}
+ *
+ * @function setCaptions
+ * @param    {Player} player
+ * @param    {Array} [captions=[{Caption}]]
+ */
+const setCaptions = (player, captions) => {
+  settings.captions = captions;
+  updateCaption(player);
+};
+
+/**
+ * Function is used to set the language of the video player.
+ *
+ * Expects { direction: 'rtl' or 'ltr' }
+ *
+ * @function setLanguage
+ * @param    {Player} player
+ * @param    {Object} [language={}]
+ */
+const setLanguage = (player, language) => {
+  settings.language = language;
+
+  // update the text direction on all caption spans
+  const videoEl = player.el();
+  const textElements = videoEl.getElementsByClassName('vjs-text');
+
+  textElements.forEach(element => element.direction = language.direction);
+};
+
+const setupCaptions = (player) => {
+  // set up divs for rendering
+  const videoEl = player.el();
+  const container = document.createElement('div');
+  const wrapper = document.createElement('div');
+  const span = document.createElement('span');
+
+  container.appendChild(wrapper);
+  wrapper.appendChild(span);
+  container.classList.add('vjs-caption-containter');
+  wrapper.classList.add('vjs-caption');
+  span.classList.add('vjs-text');
+
+  span.innerHTML = 'Testing!!';
+  videoEl.appendChild(container);
+};
 
 /**
  * Function to invoke when the player is ready.
@@ -16,6 +125,18 @@ const defaults = {};
  */
 const onPlayerReady = (player, options) => {
   player.addClass('vjs-dotsub-captions');
+
+  setupCaptions(player);
+
+  // export functions to be called externally
+  player.setCaptions = (captions) => setCaptions(player, captions);
+  player.setLanguage = (langauge) => setLanguage(player, langauge);
+  // setup event listeners a well.
+  player.on('captions', (event, data) => setCaptions(player, data));
+  player.on('language', (event, data) => setCaptions(player, data));
+  player.on('timeupdate', () => updateCaption(player));
+  // emit that the player is ready to receive captions
+  player.trigger('captionsready');
 };
 
 /**
@@ -32,7 +153,8 @@ const onPlayerReady = (player, options) => {
  */
 const dotsubCaptions = function(options) {
   this.ready(() => {
-    onPlayerReady(this, videojs.mergeOptions(defaults, options));
+    settings = videojs.mergeOptions(defaults, options);
+    onPlayerReady(this, settings);
   });
 };
 
